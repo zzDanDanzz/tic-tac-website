9340ef0886cc796b0044164daf40ddaaf1665a7e
zzDanDanzz <94631874+zzDanDanzz@users.noreply.github.com>
2022-09-17 00:04:36 +0430

    a

---
+++ _next/static/UF3Pc-rrETwhYj8F5UZ21/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/258-f105a0f5c33a6532.js","static/chunks/pages/index-b6b3112c7d1b1996.js"],"/_error":["static/chunks/pages/_error-fb68742d3cf986b6.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
